66ef3844890657faf826a8d8e3d3d5075a53cd3e
Kapil <k4p1ll.23@gmail.com>
2023-11-12 20:39:02 +0530

    Initial commit.

---
+++ src/dCritique_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { dCritique_backend } from "../../declarations/dCritique_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await dCritique_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
